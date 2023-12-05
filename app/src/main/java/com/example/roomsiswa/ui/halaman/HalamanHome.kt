4d7a08c189721f3ebd14365760ce7557541e8d9f
dhiooawp <dhio.anugerah.ft21@mail.umy.ac.id>
2023-12-06 00:39:14 +0700

    Membuat function ListSiswa dan menambahkan LazyColumn ke dalamnya

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -3,6 +3,8 @@ package com.example.roomsiswa.ui.halaman
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -100,6 +102,23 @@ fun BodyHome(
 }
 
 @Composable
-fun ListSiswa(itemSiswa: List<Siswa>, modifier: Modifier) {
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+                person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+
+@Composable
+fun DataSiswa(siswa: Siswa, modifier: Modifier) {
 
 }
