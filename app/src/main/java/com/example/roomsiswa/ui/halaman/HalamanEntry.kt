9d9a1a776c4a5cc1c1b2b5cf3406b8f71c91a45d
dhiooawp <dhio.anugerah.ft21@mail.umy.ac.id>
2023-12-05 23:57:31 +0700

    Membuat function EntrySiswaScreen

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanEntry.kt
@@ -3,7 +3,10 @@ package com.example.roomsiswa.ui.halaman
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomsiswa.R
+import com.example.roomsiswa.model.EntryViewModel
+import com.example.roomsiswa.model.PenyediaViewModel
 import com.example.roomsiswa.navigasi.DestinasiNavigasi
 
 object DestinasiEntry: DestinasiNavigasi {
@@ -11,3 +14,11 @@ object DestinasiEntry: DestinasiNavigasi {
     override val titleRes = R.string.entry_siswa
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntrySiswaScreen(
+    navigetBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+}
